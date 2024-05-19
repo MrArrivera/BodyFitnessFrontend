fcef6aae3e66fec51b47706d217431e78d957d9a
Arrivera <andresramirorivera@gmail.com>
2024-05-19 16:39:12 -0700

    Cambios front

---
+++ lib/assets/palette.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Palette{
+  // All the colors are from palette colors
+
+  // Primary color
+  static const Color primaryOrange = Color.fromARGB(255, 249,87,56);
+  static const Color secondaryOrange = Color.fromARGB(255, 238,150,75);
+
+  // Background home page color
+  static const Color white = Color.fromARGB(255, 255, 255, 255);
+
+  // Others colors
+  static const Color yellow = Color.fromARGB(255, 244,211,94);
+  static const Color darkBlue = Color.fromARGB(255, 13,59,102);
+}
